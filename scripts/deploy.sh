a1cfef70f9bff9443ee3a8f22a653c7b251683a3
Adoo <Adoo@outlook.com>
2018-02-07 10:34:25 +0800

    add echo log for deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,9 +1,13 @@
+echo "deploy types"
 cd types
 cargo package 
 cargo publish
+echo "deploy attrs"
 cd ../macros/attrs
 cargo package 
 cargo publish
+echo "deploy rundo"
+git status
 cd ../../
 cargo package 
 cargo publish
